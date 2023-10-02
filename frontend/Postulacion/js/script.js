ed33e19290269c7b3f2bea761aabf036a625146c
dst3v3n <hablack10@gmail.com>
2023-10-02 17:03:10 -0500

    corregir estructura front

--- frontend/Postulacion/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("mostrarLista").addEventListener("click", function() {
-    var lista = document.getElementById("miLista");
-    if (lista.style.display === "none") {
-      lista.style.display = "block"; // Mostrar la lista
-    } else {
-      lista.style.display = "none"; // Ocultar la lista
-    }
-  });
